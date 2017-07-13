6fce9288ab55916808b9f105e73e5d093c173d38
sagilorenzo1996 <sagilorenzo1996@gmail.com>
2017-07-13 09:23:27 +0530

    13/7/2017 9.23

---
+++ resources/views/layouts/friendsView.blade.php
@@ -0,0 +1,157 @@
+<style media="screen">
+.bar{
+
+
+}
+
+/* Box styles */
+.myBox {
+  position: absolute;
+  width: 300px;
+  margin-top: 100px;
+   right:0;
+border: none;
+padding: 5px;
+
+height: 100%;
+overflow-y: scroll;
+}
+
+/* Scrollbar styles */
+::-webkit-scrollbar {
+width: 12px;
+height: 12px;
+}
+
+::-webkit-scrollbar-track {
+border: 1px solid  #0047b3;
+border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+background:  #0047b3;
+border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+
+}
+</style>
+<div class="myBox center">
+
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+    <div class="chip">
+      Tag
+      <i class="close material-icons">close</i>
+    </div><br>
+
+</div>
+     <!-- <div class="row bar right center">
+            <div class="col m12 l12">
+              <div class="card large">
+                <div class="card-title"><br>
+                  <h3>Friends</h3>
+                </div>
+
+                <div class="card-content">
+
+
+                </div>
+                <div class="card-action">
+                  <a href="#">See more</a>
+                </div>
+
+              </div>
+            </div>
+          </div> -->
