4436164fcf26b804ca726df852d43d34c9b58933
sagilorenzo1996 <sagilorenzo1996@gmail.com>
2017-08-15 10:57:25 +0530

    15/8/2017

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -56,7 +56,6 @@ class RegisterController extends Controller
             'dob' => 'required',
             'gender' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'regpassword' => 'required|min:8|confirmed',
         ]);
     }
 
