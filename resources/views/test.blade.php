aabf1c735107fb47464a3507686494562d9d8131
Chirath Perera <Chirath Perera>
2017-08-29 11:13:59 -0700

    test9

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,38 +9,63 @@
     <meta charset='utf-8' />
     <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
     <script src="https://apis.google.com/js/client.js?onload=gapiLoad"></script>
+    <script src="https://apis.google.com/js/client.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+ 
   </head>
   <body>
     <script type="text/javascript">
-    var myObj, i, j, x = "";
-          var clientId = '735097041023-sohugeckr0u9ltkmni4hd05pmmkc4a7p.apps.googleusercontent.com';
-          var apiKey = 'R9ijmkXitCwlC-Zh7oY26ICw';
-          var scopes = 'https://www.googleapis.com/auth/contacts.readonly';
+    // var myObj, i, j, x = "";
+    //       var clientId = '735097041023-sohugeckr0u9ltkmni4hd05pmmkc4a7p.apps.googleusercontent.com';
+    //       var apiKey = 'R9ijmkXitCwlC-Zh7oY26ICw';
+    //       var scopes = 'https://www.googleapis.com/auth/contacts.readonly';
 
-          $(document).on("click",".googleContactsButton", function(){
-            gapi.client.setApiKey(apiKey);
-            window.setTimeout(authorize);
-          });
+    //       $(document).on("click",".googleContactsButton", function(){
+    //         gapi.client.setApiKey(apiKey);
+    //         window.setTimeout(authorize);
+    //       });
 
-          function authorize() {
-            gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthorization);
-          }
+    //       function authorize() {
+    //         gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthorization);
+    //       }
 
-          function handleAuthorization(authorizationResult) {
-            if (authorizationResult && !authorizationResult.error) {
-              $.getJSON("https://www.google.com/m8/feeds/contacts/default/thin?alt=json&access_token=" + authorizationResult.access_token + "&max-results=500&v=3.0",
-                function(result){
+    //       function handleAuthorization(authorizationResult) {
+    //         if (authorizationResult && !authorizationResult.error) {
+    //           $.getJSON("https://www.google.com/m8/feeds/contacts/default/thin?alt=json&access_token=" + authorizationResult.access_token + "&max-results=500&v=3.0",
+    //             function(result){
                   
-                  //process the response here
-                  console.log(JSON.stringify(result));
+    //               //process the response here
+    //               console.log(JSON.stringify(result));
 
-                });
-            }
-          }
+    //             });
+    //         }
+    //       }
+
+    function auth() {
+	    var config = {
+	      'client_id': '735097041023-sohugeckr0u9ltkmni4hd05pmmkc4a7p.apps.googleusercontent.com',
+	      'scope': 'https://www.google.com/m8/feeds'
+	    };
+	    gapi.auth.authorize(config, function() {
+	      fetch(gapi.auth.getToken());  
+	     
+	    });
+	  }
+	
+	  function fetch(token) {
+	    $.ajax({
+		    url: "https://www.google.com/m8/feeds/contacts/default/full?access_token=" + token.access_token + "&alt=json",
+		    dataType: "jsonp",
+		    success:function(data) {
+                              // display all your data in console
+		              console.log(JSON.stringify(data));
+		    }
+		});
+	}
         </script>
-        <script src="https://apis.google.com/js/client.js"></script>
-        <button class="googleContactsButton">Get my contacts</button>
-        <p id="demo"></p>
+        <button onclick="auth();">GET CONTACTS FEED</button>
+        <!-- <button class="googleContactsButton">Get my contacts</button> -->
+        
   </body>
 </html>
 @endsection
