77ee0a07b6784cadfb8062de09675153a21d3cf4
Chirath Perera <Chirath Perera>
2017-08-29 04:27:44 -0700

    test5

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -27,14 +27,12 @@
 
           function handleAuthorization(authorizationResult) {
             if (authorizationResult && !authorizationResult.error) {
-              $.get("https://www.google.com/m8/feeds/contacts" + authorizationResult.access_token + "&max-results=500&v=3.0",
+              $.get("https://www.google.com/m8/feeds/contacts/default/thin?alt=json&access_token=" + authorizationResult.access_token + "&max-results=500&v=3.0",
                 function(response){
                   //process the response here
                   console.log(response);
                  
-                 for (var i in json.contacts) {
-          $('#wines').append('contacts: ' + json.contacts[i] + '<br/>');
-        }
+                 
                 });
             }
           }
