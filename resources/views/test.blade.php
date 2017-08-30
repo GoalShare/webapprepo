e761c3f75303299976ee008657b678fa73c413d4
Chirath Perera <Chirath Perera>
2017-08-30 14:27:02 +0530

    test9

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -61,7 +61,7 @@
                               // display all your data in console
 		            console.log(JSON.stringify(data));
 
-                console.log(data.version.feed.entry.address);
+                console.log(data.version.feed.entry.gd$email.address);
 
 
 		    }
