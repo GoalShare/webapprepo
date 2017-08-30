09c6c3e054a328f05ad49aa6028df7c6d86f2a84
Chirath Perera <Chirath Perera>
2017-08-30 12:32:20 +0530

    test9

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -11,10 +11,10 @@
     <script src="https://apis.google.com/js/client.js?onload=gapiLoad"></script>
     <script src="https://apis.google.com/js/client.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
- 
+
   </head>
   <body>
-  
+
     <script type="text/javascript">
     // var myObj, i, j, x = "";
     //       var clientId = '735097041023-sohugeckr0u9ltkmni4hd05pmmkc4a7p.apps.googleusercontent.com';
@@ -34,7 +34,7 @@
     //         if (authorizationResult && !authorizationResult.error) {
     //           $.getJSON("https://www.google.com/m8/feeds/contacts/default/thin?alt=json&access_token=" + authorizationResult.access_token + "&max-results=500&v=3.0",
     //             function(result){
-                  
+
     //               //process the response here
     //               console.log(JSON.stringify(result));
 
@@ -48,11 +48,11 @@
 	//       'scope': 'https://www.google.com/m8/feeds'
 	//     };
 	//     gapi.auth.authorize(config, function() {
-	//       fetch(gapi.auth.getToken());  
-	     
+	//       fetch(gapi.auth.getToken());
+
 	//     });
 	//   }
-	
+
 	//   function fetch(token) {
 	//     $.ajax({
 	// 	    url: "https://www.google.com/m8/feeds/contacts/default/full?access_token=" + token.access_token + "&alt=json",
@@ -60,32 +60,32 @@
 	// 	    success:function(data) {
   //                             // display all your data in console
 	// 	            console.log(JSON.stringify(data));
-                  
+
   //               var data = JSON.stringify(data);
   //               var output= " ";
-                
+
   //               for (i=0; i < data.feed.length; i++){
   //                   output +=  data.feed[i].entry;
   //               }
-                
+
   //               document.write(output);
 	// 	    }
 	// 	});
 	// }
 
-    
+
   function authClick() {
-// Your Client ID is retrieved from your project 
+// Your Client ID is retrieved from your project
 //in the Developer Console => https://console.developers.google.com
     var CLIENT_ID = '735097041023-sohugeckr0u9ltkmni4hd05pmmkc4a7p.apps.googleusercontent.com';
     var SCOPES = "https://www.google.com/m8/feeds";
- 
+
     gapi.auth.authorize(
         { client_id: CLIENT_ID, scope: SCOPES, immediate: false }, authResult);
- 
+
      return false;
 }
- 
+
 /**
 * Handle response from authorization server.
 * @param {Object} authResult Authorization result.
@@ -103,13 +103,13 @@ function authResult(_Result)
         _Div.innerText = ':( Authtentication Error : ' + _Result.error;
     }
 }
- 
+
 /**
 * Load Google People client library. List Contact requested info
 */
 function loadPeopleApi()
 {
-    gapi.client.load('https://www.google.com/m8/feeds/contacts/default/full?access_token=','v3',showContacts);
+    gapi.client.load('https://www.google.com/m8/feeds/contacts','v3',showContacts);
 }
 
 /**
@@ -121,42 +121,42 @@ function showContacts()
         'resourceName': 'people/me',
             'pageSize': 100,
             'requestMask.includeField': '
-                person.phone_numbers,person.organizations,person.email_addresses,person.names'
+                contacts.phone_numbers,contacts.organizations,contacts.email_addresses,contacts.names'
         });
- 
+
     request.execute(function(resp) {
         var connections = resp.connections;
- 
+
         if (connections.length > 0)
         {
             var _Html = "<table><tr><th>Name</th><th>Email</th><th>Company</th><th>Phone</th></tr>";
             var _EmptyCell = "<td> - </td>";
- 
+
             for (i = 0; i < connections.length; i++)
             {
                 var person = connections[i];
                 _Html += "<tr>";
- 
+
                 if (person.names && person.names.length > 0)
                     _Html += "<td>" + person.names[0].displayName + "</td>";
                 else
                     _Html += _EmptyCell;
- 
+
                 if (person.emailAddresses && person.emailAddresses.length > 0)
                 _Html += "<td>" + person.emailAddresses[0].value + "</td>";
                 else
                     _Html += _EmptyCell;
- 
+
             if (person.organizations && person.organizations.length > 0)
             _Html += "<td>" + person.organizations[0].name + "</td>";
             else
             _Html += _EmptyCell;
- 
+
             if (person.phoneNumbers && person.phoneNumbers.length > 0)
             _Html += "<td>" + person.phoneNumbers[0].value + "</td>";
         else
             _Html += _EmptyCell;
- 
+
             _Html += "</tr>";
             }
             divtableresult.innerHTML = "Contacts found : <br>" + _Html;
@@ -164,7 +164,7 @@ function showContacts()
             divtableresult.innerHTML = "";
             divauthresult.innerText = "No Contacts found!";
         }
-    }); 
+    });
 }
 
         </script>
@@ -179,7 +179,7 @@ function showContacts()
     </p>
     <div id="divauthresult"></div>
     <div id="divtableresult"></div>
-        
+
   </body>
 </html>
 @endsection
