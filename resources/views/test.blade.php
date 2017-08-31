bd36db540a8255c0702f0b1f5f7be3781c7c98df
Chirath Perera <Chirath Perera>
2017-08-31 13:13:34 +0530

    test10

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -64,7 +64,7 @@
 
                 for(var i=0;i<data.feed.entry.length;i++){
                   var z=data.feed.entry[i].link[0].href;
-                document.write(+data.feed.entry[i].gd$email[0].address+'<br>');
+                document.write(data.feed.entry[i].gd$email[0].address+'<br>');
 
 
               }
