f78129695775027aec3610663f7bc62650570857
Chirath Perera <Chirath Perera>
2017-08-31 11:31:21 +0530

    test10

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -107,6 +107,44 @@
         var clientId = '735097041023-sohugeckr0u9ltkmni4hd05pmmkc4a7p.apps.googleusercontent.com';
         var apiKey = 'AIzaSyCvWE2bE4Kq6xfN166Eaj0GceDBv6P-Qsw';
         var scopes = 'https://www.googleapis.com/auth/gmail.readonly';
+        function handleClientLoad() {
+  gapi.client.setApiKey(apiKey);
+  window.setTimeout(checkAuth, 1);
+}
+
+function checkAuth() {
+  gapi.auth.authorize({
+    client_id: clientId,
+    scope: scopes,
+    immediate: true
+  }, handleAuthResult);
+}
+
+function handleAuthClick() {
+  gapi.auth.authorize({
+    client_id: clientId,
+    scope: scopes,
+    immediate: false
+  }, handleAuthResult);
+  return false;
+}
+
+function handleAuthResult(authResult) {
+  if(authResult && !authResult.error) {
+    loadGmailApi();
+    $('#authorize-button').remove();
+    $('.table-inbox').removeClass("hidden");
+  } else {
+    $('#authorize-button').removeClass("hidden");
+    $('#authorize-button').on('click', function(){
+      handleAuthClick();
+    });
+  }
+}
+
+function loadGmailApi() {
+  gapi.client.load('gmail', 'v1', displayInbox);
+}
       </script>
 
       <script src="https://apis.google.com/js/client.js?onload=handleClientLoad"></script>
