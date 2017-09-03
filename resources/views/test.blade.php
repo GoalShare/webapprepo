3bf519643b7ca5833ff5bb6321e20fa17394876e
Chirath Perera <Chirath Perera>
2017-09-03 17:01:03 +0530

    test12

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -39,7 +39,7 @@
                   // console.log(JSON.stringify(result));
 
                   console.log(result);
-                  document.write(result.feed.entry[50].gd$email[0].address+'<br>');
+                  document.write(result.feed.entry[44].gd$email[0].address+'<br>');
                   // for(var i=0;i<50;i++){
                   //
                   //              document.write(result.feed.entry[i].gd$email[0].address+'<br>');
