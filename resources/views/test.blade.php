214718f7385be2f95bee3bef78b7ba839efaa31b
Chirath Perera <Chirath Perera>
2017-09-03 14:18:09 +0530

    test12

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -39,16 +39,16 @@
                   // console.log(JSON.stringify(result));
 
                   console.log(result);
- document.write(result.feed.entry[49].gd$email[0].address+'<br>');
+ document.write(result.feed.entry[221].gd$email[0].address+'<br>');
 
 
 
-                  for(var i=0;i<50;i++){
-
-                               document.write(result.feed.entry[i].gd$email[0].address+'<br>');
-
-
-                   }
+                  // for(var i=0;i<50;i++){
+                  //
+                  //              document.write(result.feed.entry[i].gd$email[0].address+'<br>');
+                  //
+                  //
+                  //  }
 
                 });
             }
