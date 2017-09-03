2d9075fc80a58f13216ed9a8490167395617d7e7
Chirath Perera <Chirath Perera>
2017-09-03 12:06:55 +0530

    test12

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -40,7 +40,7 @@
                   console.log(result);
                   for(var i=0;i<result.feed.entry.length;i++){
 
-                                document.write(i+result.feed.entry[i]+'<br>');
+                                document.write(i+result.feed.entry[i].gd$email[0]+'<br>');
 
 
                    }
