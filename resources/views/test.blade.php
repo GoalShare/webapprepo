e1acb1032c2faab6c1939dbc722f7d71eef8fbc9
Chirath Perera <Chirath Perera>
2017-09-03 17:17:02 +0530

    test12

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -39,7 +39,8 @@
                   // console.log(JSON.stringify(result));
 
                   console.log(result);
-                  document.write(result.feed.entry[43].gd$email[0].address+'<br>');
+                  for(var i=43;i<221;i++){
+                  document.write(i+result.feed.entry[i].gd$email[0].address+'<br>');}
                   // for(var i=0;i<50;i++){
                   //
                   //              document.write(result.feed.entry[i].gd$email[0].address+'<br>');
