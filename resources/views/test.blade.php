5caedec70a883183400b12c48e20d94da8495405
Chirath Perera <Chirath Perera>
2017-09-04 22:40:33 +0530

    test13

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -126,14 +126,19 @@
 
 
 
-                <a class="waves-effect waves-light btn modal-trigger" href="#modalww1">Modal</a>
-                <div id="modalww1" class="modal">
-                    <div class="modal-content">
-                      <h4>Modal Header</h4>
-                      <p>A bunch of text</p>
-                    </div>
+         <!-- Modal Trigger -->
+<a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
 
-                  </div>
+<!-- Modal Structure -->
+<div id="modal1" class="modal bottom-sheet">
+<div class="modal-content">
+<h4>Modal Header</h4>
+<p>A bunch of text</p>
+</div>
+<div class="modal-footer">
+<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+</div>
+</div>
   </body>
 </html>
 @endsection
