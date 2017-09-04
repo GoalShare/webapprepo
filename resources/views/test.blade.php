3f718c24caa0cba8bc6639839c7f900783d11f65
Chirath Perera <Chirath Perera>
2017-09-04 20:43:50 +0530

    test12

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -41,10 +41,10 @@
                   console.log(result);
                   //for(var i=0;i<result.feed.entry.length;i++){
                   // console.log(result.feed.entry[42].gd$email);
-              
 
 
-                   console.log(includes(result.feed.entry[42].gd$email));
+
+                   console.log([result[]].includes(result.feed.entry[42].gd$email));
                   // var x=[result].includes(result.feed.entry[42].gd$email);
 
                   // for(var i=0;i<result.feed.entry.length;i++){
