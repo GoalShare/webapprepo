e00372c7e5e15e0c01a9e8d6a10abee3e753713a
Chirath Perera <Chirath Perera>
2017-09-04 08:28:06 +0530

    test12

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -43,7 +43,8 @@
                   console.log(result.feed.entry[42].gd$email);
                   console.log([result].includes(result.feed.entry[42].gd$email));
                   var x=[result].includes(result.feed.entry[42].gd$email);
-                  if(x=="false"){
+                  console.log(x);
+                  if(x=='false'){
                     console.log("izhdcnsjkncijn");
                   }
                   //!= 'undefined'
