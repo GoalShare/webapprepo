6aa5f9fc8e7108cd4f7bb085c70e85be1cf0b7ca
Chirath Perera <Chirath Perera>
2017-09-06 00:49:09 +0530

    test13

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -60,7 +60,19 @@ document.getElementById("demo").innerHTML = text;
 
         </script>
 
+        <!-- Trigger/Open The Modal -->
+        <button id="myBtn">Open Modal</button>
 
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+
+          <!-- Modal content -->
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Some text in the Modal..</p>
+          </div>
+
+        </div>
   </body>
 </html>
 @endsection
