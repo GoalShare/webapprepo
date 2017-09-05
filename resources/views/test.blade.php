d1079eb8414f5429facb9babf2cf9898e6ceb3a8
Chirath Perera <Chirath Perera>
2017-09-05 22:28:32 +0530

    test13

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -7,53 +7,14 @@
 <html>
   <head>
     <meta charset='utf-8' />
-    <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
+  
     <script src="https://apis.google.com/js/client.js?onload=gapiLoad"></script>
     <script src="https://apis.google.com/js/client.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<style>
-/* The Modal (background) */
-.modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    padding-top: 100px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
 
-/* Modal Content */
-.modal-content {
-    background-color: #fefefe;
-    margin: auto;
-    padding: 20px;
-    border: 1px solid #888;
-    width: 80%;
-}
-
-/* The Close Button */
-.close {
-    color: #aaaaaa;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-    color: #000;
-    text-decoration: none;
-    cursor: pointer;
-}
-</style>
   </head>
   <body>
-       <button class="googleContactsButton" id="myBtn">Get my contacts</button>
+       <button class="googleContactsButton">Get my contacts</button>
 
 
     <script type="text/javascript">
@@ -77,14 +38,6 @@
                 function(result){
                   console.log(result);
 
-
-                  //process the response here
-                  // console.log(JSON.stringify(result));
-
-
-                  //for(var i=0;i<result.feed.entry.length;i++){
-                  // console.log(result.feed.entry[42].gd$email);
-
                   for(var i=0;i<result.feed.entry.length;i++){
                   var x=result.feed.entry[i].gd$email;
                   if(x==undefined){
@@ -92,116 +45,19 @@
                   }
 
                   else{
-                    document.getElementById("xid").innerHTML=x[0].address;
-                  // document.write(x[0].address+'<br>');
+
+                   document.write(x[0].address+'<br>');
                   }
                 }
 
-                  //  console.log(result.feed.entry[42].gd$email);
-                  // var x=[result].includes(result.feed.entry[42].gd$email);
 
-                  // for(var i=0;i<result.feed.entry.length;i++){
-                  //   var y=[result].includes(result.feed.entry[i].gd$email);
-                  //   if(y==false){
-                  //     console.log("izhdcnsjkncijn");
-                  //   }
-                  //
-                  //
-                  // }
-
-                  //!= 'undefined'
-                  //document.write(i+result.feed.entry[42].gd$email+'<br>');//}
-                  // for(var i=0;i<50;i++){
-                  //
-                  //              document.write(result.feed.entry[i].gd$email[0].address+'<br>');
-                  //
-                  //
-                  //  }
 
                 });
             }
           }
 
-  //   function auth() {
-	//     var config = {
-	//       'client_id': '735097041023-sohugeckr0u9ltkmni4hd05pmmkc4a7p.apps.googleusercontent.com',
-	//       'scope': 'https://www.google.com/m8/feeds'
-	//     };
-	//     gapi.auth.authorize(config, function() {
-	//       fetch(gapi.auth.getToken());
-  //
-	//     });
-	//   }
-  //
-	//   function fetch(token) {
-	//     $.ajax({
-	// 	    url: "https://www.google.com/m8/feeds/contacts/default/full?access_token=" + token.access_token + "&alt=json",
-	// 	    dataType: "jsonp",
-	// 	    success:function(data) {
-  //                             // display all your data in console
-	// 	            console.log(JSON.stringify(data));
-  //               console.log(data);
-  //
-  //               for(var i=0;i<data.feed.entry.length;i++){
-  //                 var z=data.feed.entry[i].link[0].href;
-  //               document.write(data.feed.entry[i].gd$email[0].address+'<br>');
-  //
-  //
-  //             }
-  //
-  //
-	// 	    }
-	// 	});
-	// }
-
-
 
         </script>
-
-
-        <!-- The Modal -->
-        <div id="myModal" class="modal">
-
-          <!-- Modal content -->
-          <div class="modal-content">
-            <span class="close">&times;</span>
-            <script>
-            for(var xid=0;xid<result.feed.entry.length;xid++){
-            document.write('<p id="xid"></p>');}
-            </script>
-          </div>
-
-        </div>
-
-        <script>
-        // Get the modal
-        var modal = document.getElementById('myModal');
-
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-        </script>
-
-         <!-- <button onclick="auth();">GET CONTACTS FEED</button> -->
 
   </body>
 </html>
