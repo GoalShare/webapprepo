327a031d8b012c2f845cc508725946dbc7f08d89
Chirath Perera <Chirath Perera>
2017-09-06 00:27:26 +0530

    test13

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -11,34 +11,12 @@
     <script src="https://apis.google.com/js/client.js?onload=gapiLoad"></script>
     <script src="https://apis.google.com/js/client.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<style>
-.black_overlay {
-  display: none;
-  position: absolute;
-  top: 0%;
-  left: 0%;
-  width: 100%;
-  height: 100%;
-  background-color: black;
-  z-index: 1001;
-  -moz-opacity: 0.8;
-  opacity: .80;
-  filter: alpha(opacity=80);
-}
-.white_content {
-  display: none;
-  position: absolute;
-  top: 25%;
-  left: 25%;
-  width: 50%;
-  height: 50%;
-  padding: 16px;
-  border: 16px solid orange;
-  background-color: white;
-  z-index: 1002;
-  overflow: auto;
-}
-</style>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
   </head>
   <body>
        <button class="googleContactsButton">Get my contacts</button>
@@ -86,49 +64,30 @@
 
         </script>
         <script>
-        $.ajax({
-          type: 'post',
-          url: 'test2.php',
-          dataType: 'json',
-          data: {
-            txt: txtbox,
-            hidden: hiddenTxt
-          },
-          cache: false,
-          success: function(returndata) {
-            if (returndata[4] === 1) {
+  $( function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
 
-              $("#bsModal3").modal('show');
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  } );
+  </script>
 
-            } else {
-              // other code
-            }
-          },
-          error: function() {
-            console.error('Failed to process ajax !');
-          }
-        });
-        </script>
+  <div id="dialog" title="Basic dialog">
+    <p>This is an animated dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
+  </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="bsModal3" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title" id="mySmallModalLabel">Modal title</h4>
-              </div>
-              <div class="modal-body">
-                Your content goes here...
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
+  <button id="opener">Open Dialog</button>
   </body>
 </html>
 @endsection
