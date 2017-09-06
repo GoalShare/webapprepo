928a4c1a4573516598683f7e4623d237df1645f9
Chirath Perera <Chirath Perera>
2017-09-06 12:32:09 +0530

    test13

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -474,6 +474,46 @@
 
 
               </script>
+              <style>
+              /* The Modal (background) */
+              .modal {
+                  display: none; /* Hidden by default */
+                  position: fixed; /* Stay in place */
+                  z-index: 1; /* Sit on top */
+                  padding-top: 100px; /* Location of the box */
+                  left: 0;
+                  top: 0;
+                  width: 100%; /* Full width */
+                  height: 100%; /* Full height */
+                  overflow: auto; /* Enable scroll if needed */
+                  background-color: rgb(0,0,0); /* Fallback color */
+                  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+              }
+
+              /* Modal Content */
+              .modal-content {
+                  background-color: #fefefe;
+                  margin: auto;
+                  padding: 20px;
+                  border: 1px solid #888;
+                  width: 80%;
+              }
+
+              /* The Close Button */
+              .close11 {
+                  color: #aaaaaa;
+                  float: right;
+                  font-size: 28px;
+                  font-weight: bold;
+              }
+
+              .close11:hover,
+              .close11:focus {
+                  color: #000;
+                  text-decoration: none;
+                  cursor: pointer;
+              }
+              </style>
 
               <!-- The Modal -->
               <div id="myModal11" class="modal"style="z-index:4000;">
