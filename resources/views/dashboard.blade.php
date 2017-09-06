93cf0d8a085cceb09faf4f30e91da9938dcfdd65
Chirath Perera <Chirath Perera>
2017-09-06 12:48:13 +0530

    test13

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -461,8 +461,8 @@
                         }
 
                         else{
-                      
-                          text =text+document.write('<img src="img/Cornmanthe3rd-Plex-Communication-gmail.ico" height="50px" width="50px">')+x[0].address+'<br/>';
+
+                          text =text+'<img src="img/Cornmanthe3rd-Plex-Communication-gmail.ico" height="50px" width="50px">'+x[0].address+'<br/>';
 
                         }
                       }
