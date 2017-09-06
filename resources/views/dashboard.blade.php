60fc5b717baafcd0bf80dd1428f4c6ac1719b7a6
Chirath Perera <Chirath Perera>
2017-09-06 14:47:37 +0530

    test13

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -499,14 +499,16 @@
 
                 <!-- Modal content -->
                 <div class="modal-content">
+                  <div class="row" style="position:fixed;">>
                   <span class="close11 right" style="cursor:pointer;">&times;</span>
+                </div>
                   <form>
 
                   <div class="row">
 
                     <p id="demo11"></p>
                   </div>
-                <div class="row">
+                <div class="row" style="position:fixed;">
                   <div class="fixed">
 
                     <button class="btn btn left">Reset</button>
