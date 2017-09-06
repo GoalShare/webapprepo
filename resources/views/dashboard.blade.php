ebe0b1cde3c45d959c58b6301c8ab488311d62f0
Chirath Perera <Chirath Perera>
2017-09-06 11:38:23 +0530

    test13

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -470,17 +470,96 @@
 
                         else{
 
-                          document.write(x[0].address+'<br/>');
+                          text =text+x[0].address+'<br/>';
 
                         }
                       }
 
-
+                      document.getElementById("demo").innerHTML=text;
                       });
                   }
                 }
 
 
+              </script>
+              <style>
+              /* The Modal (background) */
+              .modal {
+                  display: none; /* Hidden by default */
+                  position: fixed; /* Stay in place */
+                  z-index: 1; /* Sit on top */
+                  padding-top: 100px; /* Location of the box */
+                  left: 0;
+                  top: 0;
+                  width: 100%; /* Full width */
+                  height: 100%; /* Full height */
+                  overflow: auto; /* Enable scroll if needed */
+                  background-color: rgb(0,0,0); /* Fallback color */
+                  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+              }
+
+              /* Modal Content */
+              .modal-content {
+                  background-color: #fefefe;
+                  margin: auto;
+                  padding: 20px;
+                  border: 1px solid #888;
+                  width: 80%;
+              }
+
+              /* The Close Button */
+              .close {
+                  color: #aaaaaa;
+                  float: right;
+                  font-size: 28px;
+                  font-weight: bold;
+              }
+
+              .close:hover,
+              .close:focus {
+                  color: #000;
+                  text-decoration: none;
+                  cursor: pointer;
+              }
+              </style>
+              <!-- The Modal -->
+              <div id="myModal" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+                  <span class="close right" style="cursor:pointer;">&times;</span>
+                  <p id="demo"></p>
+              <button class="btn btn-floating right">Send Request</button>
+                </div>
+
+              </div>
+
+              <script>
+              // Get the modal
+              var modal = document.getElementById('myModal');
+
+              // Get the button that opens the modal
+              var btn = document.getElementById("myBtn");
+
+              // Get the <span> element that closes the modal
+              var span = document.getElementsByClassName("close")[0];
+
+              // When the user clicks the button, open the modal
+              btn.onclick = function() {
+                  modal.style.display = "block";
+              }
+
+              // When the user clicks on <span> (x), close the modal
+              span.onclick = function() {
+                  modal.style.display = "none";
+              }
+
+              // When the user clicks anywhere outside of the modal, close it
+              window.onclick = function(event) {
+                  if (event.target == modal) {
+                      modal.style.display = "none";
+                  }
+              }
               </script>
 
           <div class="col l2 m2  center-align">
@@ -645,19 +724,7 @@
          </tr>
        </tbody>
       </table>
-      <!-- Modal Trigger -->
-<a class="waves-effect waves-light btn modal-trigger" href="#modaltest1">Modal</a>
 
-<!-- Modal Structure -->
-<div id="modaltest1" class="modal">
-<div class="modal-content">
-<h4>Modal Header</h4>
-<p>A bunch of text</p>
-</div>
-<div class="modal-footer">
-<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
-</div>
-</div>
       @foreach ($goal as $goals)
       @if ($goals->pinned==0)
           <!-- ////////////////////////////////// -->
