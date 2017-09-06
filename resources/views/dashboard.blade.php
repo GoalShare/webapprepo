77ff607bc005675c67c220d6e8e2971bbb895ba5
Chirath Perera <Chirath Perera>
2017-09-06 11:21:56 +0530

    test13

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,46 +7,8 @@
     <script src="https://apis.google.com/js/client.js?onload=gapiLoad"></script>
     <script src="https://apis.google.com/js/client.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<style>
-/* The Modal (background) */
-.modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    padding-top: 100px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
 
-/* Modal Content */
-.modal-content {
-    background-color: #fefefe;
-    margin: auto;
-    padding: 20px;
-    border: 1px solid #888;
-    width: 80%;
-}
 
-/* The Close Button */
-.close {
-    color: #aaaaaa;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-    color: #000;
-    text-decoration: none;
-    cursor: pointer;
-}
-</style>
 
 
 
@@ -460,66 +422,10 @@
   </script>
 
 
-   <script type="text/javascript">
-        var clientId = '470865305079-hn08kdr4harhu7buga04d6cuv8abnqj7.apps.googleusercontent.com';
-        var apiKey = 'AIzaSyC2F0F_mV7QnGMii66F98Rayp5Bk7kW6Ss';
-        var scopes = 'https://www.googleapis.com/auth/contacts.readonly';
-          $(document).on("click",".googleContactsButton", function(){
-            gapi.client.setApiKey(apiKey);
-            window.setTimeout(authorize);
-          });
-          function authorize() {
-            gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthorization);
-          }
-          function handleAuthorization(authorizationResult) {
-            if (authorizationResult && !authorizationResult.error) {
-              $.get("https://www.google.com/m8/feeds/contacts/default/thin?alt=json&access_token=" + authorizationResult.access_token + "&max-results=500&v=3.0",
-                function(response){
-                  //process the response here
-                  console.log(response);
-                });
-            }
-          }
-        </script>
-        <!-- The Modal -->
-        <div id="myModal" class="modal">
 
-          <!-- Modal content -->
-          <div class="modal-content">
-            <span class="close right" style="cursor:pointer;">&times;</span>
-            <p id="demo"></p>
-        <button class="btn btn-floating right">Send Request</button>
-          </div>
 
-        </div>
 
-        <script>
-        // Get the modal
-        var modal = document.getElementById('myModal');
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-        </script>
       <!--end of add goal -->
     <div class="demo-blog mdl-layout mdl-js-layout has-drawer is-upgraded">
       <main class="mdl-layout__content">
@@ -532,7 +438,7 @@
 					</div>
           <div class="col l2 m2  center-align">
             <span class=" blue-text text-lighten-1"><b>Send Invite</b></span><br>
-						<a href="#" id="myBtn" class="btn btn-floating blue lighten-1 btn-large googleContactsButton"><i class="material-icons">people</i></a>
+						<a href="#" class="btn btn-floating blue lighten-1 btn-large googleContactsButton"><i class="material-icons">people</i></a>
 					</div>
           <script type="text/javascript">
 
@@ -564,12 +470,12 @@
 
                         else{
 
-                          text =text+x[0].address+'<br/>';
+                          document.write(x[0].address+'<br/>');
 
                         }
                       }
 
-                      document.getElementById("demo").innerHTML=text;
+
                       });
                   }
                 }
