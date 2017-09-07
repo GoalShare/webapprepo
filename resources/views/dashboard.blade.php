fc00250e580d8da64fe2f59b3d08c64fdf11d895
Chirath Perera <Chirath Perera>
2017-09-07 21:46:39 +0530

    test14

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -465,10 +465,6 @@
                           text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%;"><div class="row"><div class="col l4"><input type="checkbox" value="'+x+'" id="'+i+'"/><label for="'+i+'"></label><img src="img/Cornmanthe3rd-Plex-Communication-gmail.ico" height="40px" width="40px"></div><div class="col l8 truncate">'+y.$t+'<br><span style="font-size:10px;">'+x[0].address+'</span></div></div></div></div>';
 
 
-                            console.log(i);
-
-
-
                         }
                       }
 
@@ -527,8 +523,9 @@
 
                     <div id="demo11" class="row"></div>
                       <div class="row right">
-
-
+                        <script>
+                          console.log(document.getElementById(i).id);
+                        </script>
 
                     <button class="btn" type="reset">Reset</button>
                     &nbsp&nbsp
