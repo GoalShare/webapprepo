1fd92eb6791087798ae59d6318254bd04d4e8114
Chirath Perera <Chirath Perera>
2017-09-07 21:41:51 +0530

    test14

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -463,7 +463,8 @@
                         else{
 
                           text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%;"><div class="row"><div class="col l4"><input type="checkbox" value="'+x+'" id="'+i+'"/><label for="'+i+'"></label><img src="img/Cornmanthe3rd-Plex-Communication-gmail.ico" height="40px" width="40px"></div><div class="col l8 truncate">'+y.$t+'<br><span style="font-size:10px;">'+x[0].address+'</span></div></div></div></div>';
-                          window.onload = function() {
+                          window.onload = test;
+                          function test() {
                             console.log("jhhghfgfygf");
                               }
 
