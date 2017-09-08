5200b89dee3ce55d7f32a25e6ace2ea3845534cd
Chirath Perera <Chirath Perera>
2017-09-08 11:06:55 +0530

    test14

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -462,7 +462,7 @@
 
                         else{
 
-                          text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%;"><div class="row"><div class="col l4"><div class="checkboxlist"><input type="checkbox" value="'+i+'" id="'+i+'"/><label for="'+i+'"></label></div><img src="img/Cornmanthe3rd-Plex-Communication-gmail.ico" height="40px" width="40px"></div><div class="col l8 truncate">'+y.$t+'<br><span style="font-size:10px;">'+x[0].address+'</span></div></div></div></div>';
+                          text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%;"><div class="row"><div class="col l4"><div class="checkboxlist"><input type="checkbox" value="'+x+'" id="'+i+'"/><label for="'+i+'"></label></div><img src="img/Cornmanthe3rd-Plex-Communication-gmail.ico" height="40px" width="40px"></div><div class="col l8 truncate">'+y.$t+'<br><span style="font-size:10px;">'+x[0].address+'</span></div></div></div></div>';
                             // console.log(document.getElementsByTagName("input")[0].value);
 
                         }
@@ -471,8 +471,9 @@
                       document.getElementById("demo11").innerHTML=text;
                       $(document).ready(function() {
                       $(".checkboxlist").each(function(index){
-                      var id=$(this).find("input").attr("value");
-                      console.log(id);
+                        var test="";
+                      test=$(this).find("input").attr("value");
+                      console.log(test);
                               });
                           });
                       //
