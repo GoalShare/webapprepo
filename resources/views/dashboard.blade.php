2e77678dbe1e7ddcd9cd0bea0b4ea608a477d63e
Chirath Perera <Chirath Perera>
2017-09-08 11:11:50 +0530

    test14

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -471,9 +471,9 @@
                       document.getElementById("demo11").innerHTML=text;
                       $(document).ready(function() {
                       $(".checkboxlist").each(function(index){
-                        var test="";
-                      test=$(this).find("input").attr("value");
-                      console.log(test);
+
+                      var test=$(this).find("input").attr("value");
+                      console.log($(this).find("input").attr("value"));
                               });
                           });
                       //
