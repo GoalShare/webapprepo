3cc6ce6f2fc60195b4eaeae7126333d54cfa2781
Chirath Perera <Chirath Perera>
2017-09-08 21:09:17 +0530

    test14

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -479,13 +479,13 @@
                       });
                       function Check(){
                         console.log("idhsadcbshjdncs");
-                        // $(document).ready(function() {
-                        // $(".checkboxlist").each(function(index){
-                        //   var test="";
-                        // test=$(this).find("input").attr("value");
-                        // console.log(test);
-                        //         });
-                        //     });
+                        $(document).ready(function() {
+                        $(".checkboxlist").each(function(index){
+                          var test="";
+                        test=$(this).find("input").attr("value");
+                        console.log(test);
+                                });
+                            });
                       }
                       });
                   }
