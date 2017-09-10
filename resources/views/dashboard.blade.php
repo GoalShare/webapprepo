64b0e3212c62baa7adfeb748aa4aebb4639a22ea
Chirath Perera <Chirath Perera>
2017-09-10 20:05:02 +0530

    test14

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -462,14 +462,14 @@
 
                         else{
 
-                          text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" value="'+x[0].address+'" id="'+i+'"/><label for="'+i+'"></label></span><img src="img/Cornmanthe3rd-Plex-Communication-gmail.ico" height="40px" width="40px"></div><div class="col l8 truncate">'+y.$t+'<br><span style="font-size:10px;">'+x[0].address+'</span></div></div></div></div>';
+                          text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="isSelected" value="'+x[0].address+'" id="'+i+'"/><label for="'+i+'"></label></span><img src="img/Cornmanthe3rd-Plex-Communication-gmail.ico" height="40px" width="40px"></div><div class="col l8 truncate">'+y.$t+'<br><span style="font-size:10px;">'+x[0].address+'</span></div></div></div></div>';
                             // console.log(document.getElementsByTagName("input")[0].value);
                             var invitebtn=document.getElementById('sendinv');
                              invitebtn.addEventListener("click",function(event){
                              Check();
                             });
                             function Check(){
-                              if($get(#i).checked == true){
+                              if($get("isSelected").checked == true){
                                 console.log("jdcnjksndcjsdcnjksndcjsdcn");
                               }
                               // $(document).ready(function() {
