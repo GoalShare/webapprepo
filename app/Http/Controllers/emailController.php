3d7d2014a1c3718e34c4e5302e76550f30d6a42d
Chirath Perera <Chirath Perera>
2017-09-12 12:50:37 +0530

    test14

--- app/Http/Controllers/emailController.php
+++ app/Http/Controllers/emailController.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Http\Controllers\Controller;
+use App\Models\News;
+use Request;
+use Response;
+use Input;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
