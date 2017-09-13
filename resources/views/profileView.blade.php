e6d30db10049f6c31b8e5a8bc7d788aa1d30b4f8
Chirath Perera <Chirath Perera>
2017-09-13 14:10:20 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -174,8 +174,9 @@
                         var count=0;
                             if($('[type="checkbox"]').is(":checked")){
                                console.log("qwertyuiop");
-                               console.log(($('[type="checkbox"]').is(":checked")).length);
+
                                $('input[name="checkboxnames"]:checked').each(function() {
+                                 console.log($('input[name="checkboxnames"]:checked').length);
                                   console.log(this.value);
                                   count=count+1;
                                   checkArray.push(this.value);
