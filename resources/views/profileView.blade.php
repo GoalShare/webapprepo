f30807c080335d97abf0af2b5da181133c355d7a
Chirath Perera <Chirath Perera>
2017-09-13 18:02:07 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -224,9 +224,9 @@
 
               </div>
               <div class="row right" style="position: fixed;">
-              <button class="btn" type="reset" style="position: fixed;">Reset</button>
+              <button class="btn" type="reset">Reset</button>
               &nbsp&nbsp
-              <button class="btn" id="sendinv" style="position: fixed;">Send Invite</button>
+              <button class="btn" id="sendinv">Send Invite</button>
             </div>
               </div>
 
