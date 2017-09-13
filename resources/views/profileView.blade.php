71ce01aee5070efb545e5ac19cb6042b232a41ed
Chirath Perera <Chirath Perera>
2017-09-13 19:25:15 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -148,7 +148,7 @@
 
                            }
 
-                          document.getElementById("found").innerHTML = '<p style="color:#0d47a1;font-size:17px;"><span>&nbsp&nbsp&nbsp</span>Connect with people you know on Gmail.</p>'+'<p><span>&nbsp&nbsp&nbsp&nbsp</span>We found'+" "+ count+" "+'people from your address book. Select the people you would like to connect to.</p>';
+                          document.getElementById("found").innerHTML = '<p style="color:#0d47a1;font-size:17px;"><span>&nbsp&nbsp&nbsp</span>Connect with people you know on Gmail.</p>'+'<p><span>&nbsp&nbsp&nbsp&nbsp</span>We found'+" "+ count+" "+'people from your address book. Select the people you would like to connect to.</p><hr>';
 
                       }
 
@@ -219,7 +219,7 @@
               <div class="row">
               <span id="found"></span>
               </div>
-              <hr>
+
               <!-- Modal content -->
               <div class="modal-content">
 
