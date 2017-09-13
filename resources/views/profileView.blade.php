beb8d92aa395ef557f180ffd0f6c68df0302bc58
Chirath Perera <Chirath Perera>
2017-09-14 02:00:13 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -141,25 +141,8 @@
                       }
 
                       else{
-
                         count=count+1;
-                        $.get("http://picasaweb.google.com/data/entry/api/user/"+x[0].address+"?alt=json",
-                        function(data){
-                          // var image=data.entry.gphoto$thumbnail.$t;
-                          // console.log(image);
-                          // console.log('<img src="'+data.entry.gphoto$thumbnail.$t+'">');
-                          $(data.entry.gphoto$thumbnail.$t).error(function () {
-                            $(this).css("display", "none");
-                          });
-                        // text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="'+i+'"/><label for="'+i+'"></label></span><img src="'+data.entry.gphoto$thumbnail.$t+'" height="40px" width="40px"></div><div class="col l8 truncate"><span style="font-weight: bold;">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;">'+x[0].address+'</span></div></div></div></div>';}
-                        $(data.entry.gphoto$thumbnail.$t).true(function () {
-                          $(this).console.log("csdcsd");
-                        });
-
-                        // else{
-                        //   text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="'+i+'"/><label for="'+i+'"></label></span><img src="{{asset('img/Cornmanthe3rd-Plex-Communication-gmail.ico')}}" height="40px" width="40px"></div><div class="col l8 truncate"><span style="font-weight: bold;">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;">'+x[0].address+'</span></div></div></div></div>';
-                        // }
-                      });
+                        text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="'+i+'"/><label for="'+i+'"></label></span><img src="{{asset('img/Cornmanthe3rd-Plex-Communication-gmail.ico')}}" height="40px" width="40px"></div><div class="col l8 truncate"><span style="font-weight: bold;">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;">'+x[0].address+'</span></div></div></div></div>';
                           // console.log(document.getElementsByTagName("input")[0].value);
 
 
@@ -232,7 +215,36 @@
 
             <script>
 
+            $.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.com?alt=json",
+              function(result){
+                console.log(result);
+                  var x=result.entry.gphoto$thumbnail.$t;
+                  console.log(x);
+              });
 
+              $.ajax(http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.com?alt=json, {
+  
+   statusCode: {
+      200: function (response) {
+         alert('1');
+         AfterSavedAll();
+      },
+      201: function (response) {
+         alert('1');
+         AfterSavedAll();
+      },
+      400: function (response) {
+         alert('1');
+         bootbox.alert('<span style="color:Red;">Error While Saving Outage Entry Please Check</span>', function () { });
+      },
+      404: function (response) {
+         alert('1');
+         bootbox.alert('<span style="color:Red;">Error While Saving Outage Entry Please Check</span>', function () { });
+      }
+   }, success: function () {
+      alert('1');
+   },
+});
             </script>
 
             <!-- The Modal -->
