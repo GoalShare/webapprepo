73b084bf54513da21658ef1432955c3aa660a0dd
Chirath Perera <Chirath Perera>
2017-09-13 14:28:45 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -179,7 +179,7 @@
 
                               //console.log(this.value);
 
-                                 checkArray.push(this.value);
+                                 checkArray.push('<input type="hidden" name="test'+count+'" value="'+this.value+'" />');
                               //     // document.write('<input type="hidden" name="test'+count+'" value="'+this.value+'" />');
                                //
                                   });
