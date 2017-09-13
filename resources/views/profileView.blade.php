aac561bece29c461f9a4382be8e0de8910934fa0
Chirath Perera <Chirath Perera>
2017-09-13 18:26:19 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -132,6 +132,7 @@
                     function(result){
                       console.log(result);
                       var text = '';
+                      var count=0;
                   for(var i=0;i<result.feed.entry.length;i++){
                       var x=result.feed.entry[i].gd$email;
                       var y=result.feed.entry[i].title;
@@ -140,7 +141,7 @@
                       }
 
                       else{
-
+                        count=count+1;
                         text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="'+i+'"/><label for="'+i+'"></label></span><img src="{{asset('img/Cornmanthe3rd-Plex-Communication-gmail.ico')}}" height="40px" width="40px"></div><div class="col l8 truncate">'+y.$t+'<br><span style="font-size:10px;">'+x[0].address+'</span></div></div></div></div>';
                           // console.log(document.getElementsByTagName("input")[0].value);
 
@@ -215,7 +216,8 @@
 
             <!-- The Modal -->
             <div id="myModal11" class="modal modal-fixed-footer">
-
+              <div class="row"></div>
+              <hr>
               <!-- Modal content -->
               <div class="modal-content">
 
