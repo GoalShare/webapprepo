ceab27c1c61d45d06bd1f2aa902a58e68b934d02
Chirath Perera <Chirath Perera>
2017-09-14 10:40:35 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -142,7 +142,18 @@
 
                       else{
                         count=count+1;
-                        text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="'+i+'"/><label for="'+i+'"></label></span><img src="{{asset('img/Cornmanthe3rd-Plex-Communication-gmail.ico')}}" height="40px" width="40px"></div><div class="col l8 truncate"><span style="font-weight: bold;">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;">'+x[0].address+'</span></div></div></div></div>';
+                        $.get("http://picasaweb.google.com/data/entry/api/user/"+x[0].address+"?alt=json")
+                            .done(function() {
+                              $.get("http://picasaweb.google.com/data/entry/api/user/"+x[0].address+"?alt=json",
+                                function(data){
+                                  console.log(data);
+                                    var x=data.entry.gphoto$thumbnail.$t;
+                                  
+                                    text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="'+i+'"/><label for="'+i+'"></label></span><img src="'+data.entry.gphoto$thumbnail.$t+'" height="40px" width="40px"></div><div class="col l8 truncate"><span style="font-weight: bold;">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;">'+x[0].address+'</span></div></div></div></div>';
+
+                        });
+                      });
+
                           // console.log(document.getElementsByTagName("input")[0].value);
 
 
@@ -213,7 +224,7 @@
             }
             </script>
 
-            <script>
+            <!-- <script>
 
 
 $.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.com?alt=json")
@@ -231,7 +242,7 @@ $.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.c
     });
 
 
-            </script>
+            </script> -->
 
 
             <!-- The Modal -->
