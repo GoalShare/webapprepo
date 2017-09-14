fda26b83ca01be749c26439417e52e3c74a1f75a
Chirath Perera <Chirath Perera>
2017-09-14 10:22:03 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -215,20 +215,20 @@
 
             <script>
 
-            $.get("http://picasaweb.google.com/data/entry/api/user/qeuniversityreach@pearson.com?alt=json",
-              function(data){
-                console.log(data);
-                  var x=data.entry.gphoto$thumbnail.$t;
-                  console.log(x);
+
+$.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.com.com?alt=json")
+    .done(function(data) {
 
 
-});
-$.get("http://picasaweb.google.com/data/entry/api/user/qeuniversityreach@pearson.com?alt=json")
-    .done(function() {
-      console.log("dgvcjndcjkndc");
+          console.log(data);
+            var x=data.entry.gphoto$thumbnail.$t;
+            console.log(x);
+
+
+
     }).fail(function() {
       console.log("wefdsdvcsdvcsdzcsd");
-    })
+    });
 
 
             </script>
