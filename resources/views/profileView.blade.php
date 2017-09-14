81d9bf36a7b34e84d8faf938f52e67f93b01cfd5
Chirath Perera <Chirath Perera>
2017-09-15 00:45:30 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -175,7 +175,7 @@
                           // console.log(v);
                           if(value==v){
                             console.log(v+i);
-                            // console.log($('[type="checkbox"]').value);
+                            console.log($('[type="checkbox"]').attr(i));
                           }
                         @endforeach
                       }
