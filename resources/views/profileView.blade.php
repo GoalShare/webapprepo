8890bf010e136c4ae0cdd09bfe05e44978bdf5c5
Chirath Perera <Chirath Perera>
2017-09-14 10:45:40 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -142,18 +142,7 @@
 
                       else{
                         count=count+1;
-                        $.get("http://picasaweb.google.com/data/entry/api/user/"+x[0].address+"?alt=json")
-                            .done(function() {
-                              $.get("http://picasaweb.google.com/data/entry/api/user/"+x[0].address+"?alt=json",
-                                function(data){
-                                  console.log(data);
-                                    var x=data.entry.gphoto$thumbnail.$t;
-                                  
-                                    text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="'+i+'"/><label for="'+i+'"></label></span><img src="'+data.entry.gphoto$thumbnail.$t+'" height="40px" width="40px"></div><div class="col l8 truncate"><span style="font-weight: bold;">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;">'+x[0].address+'</span></div></div></div></div>';
-
-                        });
-                      });
-
+                        text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="'+i+'"/><label for="'+i+'"></label></span><img src="{{asset('img/Cornmanthe3rd-Plex-Communication-gmail.ico')}}" height="40px" width="40px"></div><div class="col l8 truncate"><span style="font-weight: bold;">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;">'+x[0].address+'</span></div></div></div></div>';
                           // console.log(document.getElementsByTagName("input")[0].value);
 
 
@@ -224,25 +213,39 @@
             }
             </script>
 
-            <!-- <script>
+            <script>
 
+//             $.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.com?alt=json",
+//               function(data){
+//                 console.log(data);
+//                   var x=data.entry.gphoto$thumbnail.$t;
+//                   console.log(x);
+//
+//
+// });
+// $.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.com?alt=json")
+//     .done(function() {
+//       console.log("dgvcjndcjkndc");
+//     }).fail(function() {
+//       console.log("wefdsdvcsdvcsdzcsd");
+//     });
 
-$.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.com?alt=json")
-    .done(function() {
-      $.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.com?alt=json",
-        function(data){
-          console.log(data);
-            var x=data.entry.gphoto$thumbnail.$t;
-            document.write('<img src="'+data.entry.gphoto$thumbnail.$t+'">');
+    var success = false;
 
-
-});
-    }).fail(function() {
-      console.log("wefdsdvcsdvcsdzcsd");
+    $.getJSON(http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.com?alt=json, function(json) {
+        success = true;
+        console.log("ssssss");
     });
 
-
-            </script> -->
+    // Set a 5-second (or however long you want) timeout to check for errors
+    setTimeout(function() {
+        if (!success)
+        {
+            // Handle error accordingly
+            alert("Yup, 404"); // string was "Houston, we have a problem."
+        }
+    }, 5000);
+            </script>
 
 
             <!-- The Modal -->
