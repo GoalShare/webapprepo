5d2e121920f81052b089b66769d22483ccf13e2c
Chirath Perera <Chirath Perera>
2017-09-15 22:53:08 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -296,7 +296,7 @@ $.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.c
                 <div id="found1" style="height:25px;background-color:#0d47a1;color:white;"><img src="{{asset('img/Martz90-Circle-Gmail.png')}}" height="20px" width="20px">&nbsp Gmail</div>
                 <div id="found2" style="height:25px;"></div>
                 <div id="found3" style="height:25px;"></div><br>
-                <div style="background-color:#EDEEEE;"><span><span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span><input type="checkbox" id="chk" onclick="toggle(this);" /><label for="chk"></label>Select all<input style="max-width:25px;" type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."></span></div>
+                <div style="background-color:#EDEEEE;"><span><span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</span><input type="checkbox" id="chk" onclick="toggle(this);" /><label for="chk"></label>Select all<input style="max-width:50px;max-height:10px;" class="right" type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."></span></div>
                 <script>
                 function toggle(source) {
                   var checkboxes = document.querySelectorAll('input[type="checkbox"]');
