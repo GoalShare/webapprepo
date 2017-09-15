8bb9ea5643ed875472f53f54ba60bd682a3e46d5
Chirath Perera <Chirath Perera>
2017-09-15 14:01:30 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -196,16 +196,20 @@
 
 
 
+
+
                       function toggle(source) {
                         var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-                        
                           for (var i = 0; i < checkboxes.length; i++) {
                               if (checkboxes[i] != source)
                                 checkboxes[i].checked = source.checked;
                               }
                             }
+                  
 
 
+                    //
+                    //
                     var invitebtn=document.getElementById('sendinv');
                      invitebtn.addEventListener("click",function(event){
                      Check();
