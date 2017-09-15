dd64de2241b4c3e236341ce45073cf72338b044f
Chirath Perera <Chirath Perera>
2017-09-15 14:06:36 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -300,6 +300,7 @@ $.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.c
                 <script>
                 function toggle(source) {
                   var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+                  console.log(checkboxes);
                     for (var i = 0; i < checkboxes.length; i++) {
                         if (checkboxes[i] != source)
                           checkboxes[i].checked = source.checked;
