8e1c3e209838827e3f5df22b2ded72de2af5e719
Chirath Perera <Chirath Perera>
2017-09-15 09:08:52 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -179,7 +179,7 @@
                           if(value==v){
 
                             console.log(v+" "+k);
-                            console.log(document.getElementById("ch"+k).disabled=true);
+                            document.getElementById("ch"+k).disabled=true;
                           }
 
                         @endforeach
