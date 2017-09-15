31dbdd39f8aeea045f98fde0fcda482ab07ef9ea
Chirath Perera <Chirath Perera>
2017-09-15 09:40:23 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -179,7 +179,7 @@
                           if(value==v){
 
                             console.log(v+" "+k);
-                            console.log($('#ch'+k)[0].disabled=true);
+                            console.log($('#ch'+k)[0].$('input[name="checkboxnames"]').disabled=true);
                           }
 
                         @endforeach
