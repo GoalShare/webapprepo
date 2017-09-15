cf2f5bdac6bf9b5aea440eecd4d701da21f17c42
Chirath Perera <Chirath Perera>
2017-09-15 13:58:45 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -197,7 +197,12 @@
 
 
                       function toggle(source) {
-                        console.log(document.querySelectorAll('input[type="checkbox"]'));
+                        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+                        console.log(checkboxes);
+                          for (var i = 0; i < checkboxes.length; i++) {
+                              if (checkboxes[i] != source)
+                                checkboxes[i].checked = source.checked;
+                              }
                             }
 
 
