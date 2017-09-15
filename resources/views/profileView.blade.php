d59222a979d5853ef54f8f182270d2d9d053de02
Chirath Perera <Chirath Perera>
2017-09-15 13:59:52 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -198,7 +198,7 @@
 
                       function toggle(source) {
                         var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-                        console.log(checkboxes);
+                        
                           for (var i = 0; i < checkboxes.length; i++) {
                               if (checkboxes[i] != source)
                                 checkboxes[i].checked = source.checked;
