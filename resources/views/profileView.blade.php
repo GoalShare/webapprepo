3e4c13e92f8eea930c8851bb3dd76ae184fb124e
Chirath Perera <Chirath Perera>
2017-09-15 14:27:01 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -302,11 +302,11 @@ $.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.c
                   var checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
                     for (var i = 0; i < checkboxes.length; i++) {
-                      if(checkboxes[i]== "disabled"){
+                      if(checkboxes[i]== disabled){
                         console.log("jndcjndms");
                       }
-                        if (checkboxes[i] != source)
-                          checkboxes[i].checked = source.checked;
+                        // if (checkboxes[i] != source)
+                        //   checkboxes[i].checked = source.checked;
                         }
                       }
                 </script>
