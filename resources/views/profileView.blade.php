d014e2e907374316470815172a999644cff98a4e
Chirath Perera <Chirath Perera>
2017-09-17 23:19:09 +0530

    test14

--- resources/views/profileView.blade.php
+++ resources/views/profileView.blade.php
@@ -150,7 +150,7 @@
                             console.log(x[0].address+" "+i);
                             // console.log($('#ch'+k)[0]);
 
-                            text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="ch'+i+'" disabled/><label for="ch'+i+'"></label></span><img src="{{asset('img/Cornmanthe3rd-Plex-Communication-gmail.ico')}}" height="40px" width="40px"></div><div class="col l8 truncate"><span style="font-weight: bold;">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;">'+x[0].address+'</span></div></div></div></div>';
+                            text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="ch'+i+'" disabled/><label for="ch'+i+'"></label></span><img src="{{asset('img/Cornmanthe3rd-Plex-Communication-gmail.ico')}}" height="40px" width="40px"></div><div class="col l8 truncate"><span style="font-weight: bold;" class="tosearch">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;" class="tosearch">'+x[0].address+'</span></div></div></div></div>';
                             set=1;
 
                           }
@@ -160,7 +160,7 @@
                         @endforeach
 
                         if(set==0){
-                          text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="ch'+i+'"/><label for="ch'+i+'"></label></span><img src="{{asset('img/Cornmanthe3rd-Plex-Communication-gmail.ico')}}" height="40px" width="40px"></div><div class="col l8 truncate"><span <span style="font-weight: bold;">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;">'+x[0].address+'</span></div></div></div></div>';
+                          text =text+'<div class="col l6"><div class="card" style="width:100%; height:100%;max-height:100%; background-color: #EEEEEE;"><div class="row"><div class="col l4"><span class="checkboxlist"><input type="checkbox" name="checkboxnames" value="'+x[0].address+'" id="ch'+i+'"/><label for="ch'+i+'"></label></span><img src="{{asset('img/Cornmanthe3rd-Plex-Communication-gmail.ico')}}" height="40px" width="40px"></div><div class="col l8 truncate"><span <span style="font-weight: bold;" class="tosearch">'+y.$t+'</span><br><span style="font-size:12px;color:#A7A7A7;" class="tosearch">'+x[0].address+'</span></div></div></div></div>';
 
                         }
 
@@ -321,7 +321,12 @@ $.get("http://picasaweb.google.com/data/entry/api/user/chirathpereraz1st@gmail.c
 
                       function myFunction() {
                         console.log("dcisdjcnsjkdcnkdmn");
+                        var key = this.value.toUpperCase();
+                        $(".tosearch").each(function() {
+                          var $this = $(this);
+                          $this.toggle($(this).text().indexOf(key) >= 0);
 
+     });
                       }
 </script>
               </div>
