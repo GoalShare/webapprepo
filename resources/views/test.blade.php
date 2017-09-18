919bbf038dedaddf01014e793199e2c199fffa3e
Chirath Perera <Chirath Perera>
2017-09-18 16:53:27 +0530

    test14

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -14,11 +14,12 @@
 <script src="http://connect.facebook.net/en_US/all.js"></script>
 
 
-<a href="fb-messenger://share/?link= https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fsharing%2Freference%2Fsend-dialog&app_id=123456789">Send In Messenger</a>
+<a onclick="test();">Send In Messenger</a>
 
 <script language="javascript" type="text/javascript">
+function test(){
 window.open('fb-messenger://share?link=' + encodeURIComponent(www.lifewithgoals.com) + '&app_id=' + encodeURIComponent(284837855364891));
-
+}
 </script>
 
 
