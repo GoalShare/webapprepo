5f4e704b4a4c8fa5b639949e7f601d6ee851413a
Chirath Perera <Chirath Perera>
2017-09-18 20:42:38 +0530

    test14

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,9 +5,16 @@
 
 <!DOCTYPE html>
 <html>
-  <head>
-
-  </head>
+<head>
+<title>Your Website Title</title>
+  <!-- You can use open graph tags to customize link previews.
+  Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
+<meta property="og:url"           content="http://www.lifewithgoals.com/test" />
+<meta property="og:type"          content="website" />
+<meta property="og:title"         content="Your Website Title" />
+<meta property="og:description"   content="Your description" />
+<meta property="og:image"         content="{{asset('favicon/android-chrome-256x256.png')}}" />
+</head>
   <body>
 
 
