273049ffa0df092c9b9d22949145c61f7c42eb69
Chirath Perera <Chirath Perera>
2017-09-18 16:51:40 +0530

    test14

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -14,39 +14,10 @@
 <script src="http://connect.facebook.net/en_US/all.js"></script>
 
 
-<button onclick="ShowMyName()">test</button>
+<a href="fb-messenger://share/?link= https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fsharing%2Freference%2Fsend-dialog&app_id=123456789">Send In Messenger</a>
 
 <script language="javascript" type="text/javascript">
-function ShowMyName() {
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '284837855364891',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v2.10'
-    });
-    FB.AppEvents.logPageView();
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-  FB.ui({
-    method: 'send',
-    link: 'http://www.facebook.com/dialog/send?app_id=123456789&amp;link=http://www.nytimes.com/interactive/2015/04/15/travel/europe-favorite-streets.html&amp;redirect_uri=https://www.domain.com/',
-    app_id:'284837855364891',
-    redirect_uri:'http://www.lifewithgoals.com/test',
-    display:'popup',
-    to:'',
-
-  });
-
-    }
+window.open('fb-messenger://share?link=' + encodeURIComponent(www.lifewithgoals.com) + '&app_id=' + encodeURIComponent(284837855364891));
 
 </script>
 
