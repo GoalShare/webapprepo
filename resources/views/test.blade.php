6f92db8de3eb32784466c8d47f7ea8624b425988
Chirath Perera <Chirath Perera>
2017-09-18 11:31:02 +0530

    test14

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,142 +6,34 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset='utf-8' />
 
-    <script src="https://apis.google.com/js/client.js?onload=gapiLoad"></script>
-    <script src="https://apis.google.com/js/client.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<style>
-/* The Modal (background) */
-.modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    padding-top: 100px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-    background-color: #fefefe;
-    margin: auto;
-    padding: 20px;
-    border: 1px solid #888;
-    width: 80%;
-}
-
-/* The Close Button */
-.close {
-    color: #aaaaaa;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-}
-
-.close:hover,
-.close:focus {
-    color: #000;
-    text-decoration: none;
-    cursor: pointer;
-}
-</style>
   </head>
   <body>
-       <button class="googleContactsButton" id="myBtn">Get my contacts</button>
 
-
-    <script type="text/javascript">
-
-          var clientId = '735097041023-sohugeckr0u9ltkmni4hd05pmmkc4a7p.apps.googleusercontent.com';
-          var apiKey = 'R9ijmkXitCwlC-Zh7oY26ICw';
-          var scopes = 'https://www.googleapis.com/auth/contacts.readonly';
-
-          $(document).on("click",".googleContactsButton", function(){
-            gapi.client.setApiKey(apiKey);
-            window.setTimeout(authorize);
-          });
-
-          function authorize() {
-            gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthorization);
-          }
-
-          function handleAuthorization(authorizationResult) {
-            if (authorizationResult && !authorizationResult.error) {
-              $.get("https://www.google.com/m8/feeds/contacts/default/thin?alt=json&access_token=" + authorizationResult.access_token + "&max-results=500&v=3.0",
-                function(result){
-                  console.log(result);
-                  var text = "";
-              for(var i=0;i<result.feed.entry.length;i++){
-                  var x=result.feed.entry[i].gd$email;
-
-                  if(x==undefined){
-                    console.log("hjhjdbcsjhdbchs");
-                  }
-
-                  else{
-
-                    text =text+x[0].address+'<br/>';
-
-                  }
-                }
-
-                document.getElementById("demo").innerHTML=text;
-                });
-            }
-          }
-
-
-        </script>
-
-
-
-
-  <!-- The Modal -->
-  <div id="myModal" class="modal">
-
-    <!-- Modal content -->
-    <div class="modal-content">
-      <span class="close right" style="cursor:pointer;">&times;</span>
-      <p id="demo"></p>
-  <button class="btn btn-floating right">Send Request</button>
-    </div>
-
-  </div>
-
-  <script>
-  // Get the modal
-  var modal = document.getElementById('myModal');
-
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
-
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks the button, open the modal
-  btn.onclick = function() {
-      modal.style.display = "block";
-  }
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-      modal.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  }
-  </script>
-
+<div id="facebook_invite"></div>
+<script src="http://connect.facebook.net/en_US/all.js"></script>
+<script>
+ FB.init({
+  appId:'130572204252840',
+  cookie:true,
+  status:true,
+  xfbml:true
+ });
+</script>
+<script>
+ function FBInvite(){
+  FB.ui({
+   method: 'apprequests',
+   message: 'Invite your Facebook Friends'
+  },function(response) {
+   if (response) {
+    alert('Successfully Invited');
+   } else {
+    alert('Failed To Invite');
+   }
+  });
+ }
+</script>
   </body>
 </html>
 @endsection
