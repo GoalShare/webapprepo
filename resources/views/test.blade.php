16dda1f4e747eab2d757f9a4cc8fdd76b4bc5c9a
Chirath Perera <Chirath Perera>
2017-09-18 20:36:37 +0530

    test14

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -14,8 +14,8 @@
 <script src="http://connect.facebook.net/en_US/all.js"></script>
 
 
-<div class="fb-send" data-href="http://www.lifewithgoals.com/"></div>
-
+<div style="height:50px;" data-layout="button_count" class="fb-send" data-href="http://www.lifewithgoals.com/"></div>
+<div id="fb-root"></div>
 <script language="javascript" type="text/javascript">
 
   window.fbAsyncInit = function() {
@@ -28,25 +28,18 @@
     FB.AppEvents.logPageView();
   };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
+  (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
 
 </script>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=284837855364891";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+
+
 
 
   </body>
