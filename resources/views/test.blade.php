89bea12f9e27b62859fc44479efabd56f830201c
Chirath Perera <Chirath Perera>
2017-09-18 17:06:20 +0530

    test14

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -14,13 +14,39 @@
 <script src="http://connect.facebook.net/en_US/all.js"></script>
 
 
-<button onclick="test();">Send In Messenger</button>
+<div class="fb-send" data-href="https://developers.facebook.com/docs/plugins/"></div>
 
 <script language="javascript" type="text/javascript">
-function test(){
-window.open('fb-messenger://share?link=' + encodeURIComponent("www.lifewithgoals.com") + '&app_id=' + encodeURIComponent("284837855364891"));
-}
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '284837855364891',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.10'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
+
 </script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=284837855364891";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
 
   </body>
