1a041da8f3adeebf73eb3b7427432269d079e093
chirath93 <chirathpereraz1st@gmail.com>
2017-09-19 14:03:42 +0530

    test14

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -31,7 +31,45 @@
 
 
 <button><script type="in/Login"></script></button>
-<script>IN.User.authorize(callbackFunction, callbackScope);</script>
+<script>
+IN.User.authorize(callbackFunction, callbackScope);
+<script type="text/javascript">
+
+  // Setup an event listener to make an API call once auth is complete
+    function onLinkedInLoad() {
+      IN.Event.on(IN, "auth", shareContent);
+    }
+
+  // Handle the successful return from the API call
+  function onSuccess(data) {
+    console.log(data);
+  }
+
+  // Handle an error response from the API call
+  function onError(error) {
+    console.log(error);
+  }
+
+  // Use the API call wrapper to share content on LinkedIn
+  function shareContent() {
+
+    // Build the JSON payload containing the content to be shared
+    var payload = {
+      "comment": "Check out developer.linkedin.com! http://linkd.in/1FC2PyG",
+      "visibility": {
+        "code": "anyone"
+      }
+    };
+
+    IN.API.Raw("/people/~/shares?format=json")
+      .method("POST")
+      .body(JSON.stringify(payload))
+      .result(onSuccess)
+      .error(onError);
+  }
+
+</script>
+</script>
 
 
 <script src="http://connect.facebook.net/en_US/all.js"></script>
