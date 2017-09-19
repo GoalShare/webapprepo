74ec11b617c49aa028266e0c4fbead750033b675
chirath93 <chirathpereraz1st@gmail.com>
2017-09-19 14:10:49 +0530

    test14

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -29,44 +29,8 @@
 </head>
   <body>
 
-
-<script type="in/Login"></script>
-<script>
-
-  // Setup an event listener to make an API call once auth is complete
-    function onLinkedInLoad() {
-      IN.Event.on(IN, "auth", shareContent);
-    }
-
-  // Handle the successful return from the API call
-  function onSuccess(data) {
-    console.log(data);
-  }
-
-  // Handle an error response from the API call
-  function onError(error) {
-    console.log(error);
-  }
-
-  // Use the API call wrapper to share content on LinkedIn
-  function shareContent() {
-
-    // Build the JSON payload containing the content to be shared
-    var payload = {
-      "comment": "Check out developer.linkedin.com! http://linkd.in/1FC2PyG",
-      "visibility": {
-        "code": "anyone"
-      }
-    };
-
-    IN.API.Raw("/people/~/shares?format=json")
-      .method("POST")
-      .body(JSON.stringify(payload))
-      .result(onSuccess)
-      .error(onError);
-  }
-
-</script>
+    <script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
+    <script type="IN/Share" data-url="http://www.lifewithgoals.com/" data-counter="top"></script>
 </script>
 
 
