2097abe7737eda16b3f7939f173f41e241030c46
chirath <chirathpereraz1st@gmail.com>
2017-11-03 13:28:12 +0530

    test18

--- resources/views/youtube.blade.php
+++ resources/views/youtube.blade.php
@@ -7,56 +7,7 @@
       <title>YouTube API Uploads via CORS</title>
 
       <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Open+Sans' type='text/css'>
-      <style>
-      #channel-image {
-  width: 2em;
-  height: 2em;
-  vertical-align: middle;
-}
 
-#channel-name {
-  margin-left: 0.2em;
-  margin-right: 0.2em;
-}
-
-#disclaimer {
-  font-size: 0.75em;
-  color: #aeaeae;
-  max-width: 350px;
-}
-
-body {
-  font-family: "Open Sans", sans-serif;
-  font-size: 1.5em;
-}
-
-.post-sign-in {
-  display: none;
-}
-
-.during-upload {
-  display: none;
-}
-
-.post-upload {
-  display: none;
-}
-
-label {
-  display: block;
-}
-
-input[type="text"], textarea, progress {
-  font-size: 0.75em;
-  width: 15em;
-  margin-bottom: 1em;
-  padding: 0.5em;
-  font-family: "Open Sans", sans-serif;
-}
-
-textarea {
-  height: 7em;
-}
 
       </style>
     </head>
